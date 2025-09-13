b214fdb6966375b51d422312fca924c7358960a2
Christina Trochidou <xristinatrox@yahoo.gr>
2025-09-13 11:31:55 +0300

    Add JS to populate display when digit buttons are clicked

--- script.js
+++ script.js
@@ -18,3 +18,33 @@ function subtract(a , b) {
     }
     return a / b;
  }
+
+/* 2.Checking its functionality
+ const firstNumber = 3;
+ const operator = "+";
+ const secondNumber = 5;
+
+ add(firstNumber, secondNumber);
+ console.log(add(firstNumber, secondNumber)); */
+
+ function operate(operator, a, b) {
+    if (operator === "+") return add(a, b);
+    if (operator === "-") return subtract(a, b);
+    if (operator === "*") return multiply(a, b);
+    if (operator === "/") return divide(a, b);
+ } 
+  
+  let currentInput = "";
+
+  const display = document.getElementById("display");
+  const digitButtons = document.querySelectorAll(".digit");
+
+  function updateDisplay(digit) { 
+   currentInput += digit;
+   display.value = currentInput;
+ }
+    digitButtons.forEach(button => {
+      button.addEventListener( "click", () => {
+      updateDisplay(button.textContent);
+    });
+   });

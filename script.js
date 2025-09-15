4452c68d72b8b2060e662d3a55d0e5b7e8314c6b
Christina Trochidou <xristinatrox@yahoo.gr>
2025-09-15 12:44:08 +0300

    Add full calculator functionality

--- script.js
+++ script.js
@@ -34,17 +34,60 @@ function subtract(a , b) {
     if (operator === "/") return divide(a, b);
  } 
   
-  let currentInput = "";
+  
 
   const display = document.getElementById("display");
   const digitButtons = document.querySelectorAll(".digit");
+  const operatorButtons = document.querySelectorAll(".operator");
+  const equals = document.querySelector(".equals");
 
+  /* Initial check of the functionality of the display
+  let currentInput = "";
   function updateDisplay(digit) { 
    currentInput += digit;
    display.value = currentInput;
- }
+ } */
+
+   let firstOperand = "";
+   let operator = "";
+   let secondOperand = "";
+   let displayValue = '0';
+   let waitingForSecondOperand = false;
+
+    function updateDisplay() {
+
+    display.value = displayValue;
+
+     }
+
     digitButtons.forEach(button => {
       button.addEventListener( "click", () => {
-      updateDisplay(button.textContent);
+      const digit = button.textContent;
+       if (waitingForSecondOperand) {
+         displayValue = digit;
+         waitingForSecondOperand = false;
+       } else {
+         displayValue = displayValue === '0' ? digit : displayValue + digit;
+       }
+       updateDisplay();
     });
+   }); 
+
+     operatorButtons.forEach(button => {
+     button.addEventListener( "click", () => {
+      if (firstOperand === "") { 
+        firstOperand = displayValue;
+        operator = button.textContent;
+        waitingForSecondOperand = true;
+   }
    });
+  });
+
+   equals.addEventListener( "click", () => {
+        secondOperand = displayValue;
+        result = operate(operator, firstOperand,secondOperand);
+        displayValue = result;
+        updateDisplay();
+   });
+  
+   
